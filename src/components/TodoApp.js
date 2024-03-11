5566c2ebab82c7f4e41ac0cbf32a742098405649
Pradnya136 <pradnyasonawane133@gmail.com>
2024-03-11 23:47:28 +0530

    no major changes

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 
 const TodoApp = () => {
-  // State for storing todos
+  
   const [todos, setTodos] = useState([]);
   
-  // State for storing current todo input
+
   const [todoInput, setTodoInput] = useState('');
 
-  // Function to handle adding a todo
+
   const addTodo = () => {
     if (todoInput.trim() !== '') {
       setTodos([...todos, todoInput]);
@@ -15,7 +15,6 @@ const TodoApp = () => {
     }
   };
 
-  // Function to handle removing a todo
   const removeTodo = (index) => {
     const updatedTodos = todos.filter((todo, i) => i !== index);
     setTodos(updatedTodos);
@@ -24,21 +23,21 @@ const TodoApp = () => {
   return (
     <div>
       <h1>To-Do List</h1>
-      {/* Input field for adding a new todo */}
+   
       <input 
         type="text" 
         value={todoInput} 
         onChange={(e) => setTodoInput(e.target.value)} 
         placeholder="Add a new todo" 
       />
-      {/* Button to add a new todo */}
+
       <button onClick={addTodo}>Add Todo</button>
-      {/* List of todos */}
+  
       <ul>
         {todos.map((todo, index) => (
           <li key={index}>
             {todo}
-            {/* Button to remove a todo */}
+    
             <button onClick={() => removeTodo(index)}>Remove</button>
           </li>
         ))}
